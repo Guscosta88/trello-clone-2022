361ed8fa8ae1c02380e054e822a8ac614ddcf78a
Guscosta88 <guslfcosta@gmail.com>
2022-10-13 14:21:51 +1100

    mashmallow added

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_marshmallow import Marshmallow
+
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -66,6 +70,98 @@ def seed_db():
     db.session.add_all(cards)
     db.session.commit()
     print('Tables seeded')
+
+
+# Schema
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "description", "status", "priority", "date")
+        ordered = True
+
+# cards_schema = CardSchema()
+# cards_schema = CardSchema(many=True)
+
+
+
+#Old Legacy applications Alchemy Method
+
+# @app.cli.command('all_cards')
+# def all_cards():
+# #Select * from cards; vvv Equivalent
+#     cards = Card.query.all()
+# #to return cards.
+#     print(cards[0].__dict__)
+
+
+
+# @app.cli.command('first_cards')
+# def first_cards():
+# #Select first card only; vvv Equivalent
+#     card = Card.query.first()
+# #to return cards.
+#     print(card.__dict__)
+
+
+#New Alchemy Method
+
+# @app.cli.command('all_cards')
+# def all_cards():
+#     stmt = db.select(Card.title, Card.status)
+#     # cards = db.session.execute(stmt)
+#     cards = db.session.execute(stmt)
+#     for card in cards:
+#         # print(card.title)
+#         print(card)
+#     # print(cards.all())
+
+
+# @app.cli.command('all_cards')
+# def all_cards():
+#     stmt = db.select(Card).where(Card.id > 2)
+#     cards = db.session.scalars(stmt)
+#     for card in cards:
+#         print(card.__dict__)
+
+
+# @app.cli.command('all_cards')
+# def all_cards():
+#     stmt = db.select(Card).where(db.or_(Card.status == 'To Do', Card.priority == 'High'))
+#     cards = db.session.scalars(stmt)
+#     for card in cards:
+#         print(card.__dict__)
+
+# @app.cli.command('all_cards')
+# def all_cards():
+#     stmt = db.select(Card).filter_by(status='To Do')
+#     cards = db.session.scalars(stmt)
+#     for card in cards:
+#         print(card.__dict__)
+
+# @app.cli.command('all_cards')
+# def all_cards():
+#     stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
+#     cards = db.session.scalars(stmt)
+#     for card in cards:
+#         print(card.title, card.priority)
+
+# @app.cli.command('count_outgoing')
+# def count_ongoing():
+#     stmt = db.select(db.func.count()).select_from(Card).filter_by(status='Ongoing')
+#     cards = db.session.scalar(stmt)
+#     print(cards)
+
+@app.route('/cards/', methods=["GET"])
+def all_cards():
+    stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
+    cards = db.session.scalars(stmt)
+    return CardSchema(many=True).dump(cards)
+
+
+@app.cli.command('first_card')
+def first_card():
+    stmt = db.select(Card).limit(1)
+    card = db.session.scalar(stmt)
+    print(card.__dict__)
 
 
 @app.route('/')
